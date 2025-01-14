75cb4c35b8b19c2652310e90313b7f84036f56d3
onethousendkproejcet <156320579+onethousendkproejcet@users.noreply.github.com>
2025-01-14 22:52:55 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 module.exports = {
 	helpCmdPerPage: 10, //- Number of commands per page of help command
 	lyricsMaxResults: 5, //- Number of results for lyrics command (Do not touch this value if you don't know what you are doing)
-	adminId: "UserId", //- Replace UserId with the Discord ID of the admin of the bot
-	token: process.env.token || "", //- Bot's Token
+	adminId: "294803602273206274", //- Replace UserId with the Discord ID of the admin of the bot
+	token: process.env.token || "MTMyODgxODIwMTY3MjQxNzM0MA.Go_-gE.4gr_Cjn5U9YNnCffKB1rZP9QY56QPF2iN1UW74", //- Bot's Token
 	clientId: process.env.clientId || "", //- ID of the bot
 	clientSecret: process.env.clientSecret || "", //- Client Secret of the bot
 	port: 4200, //- Port of the API and Dashboard
